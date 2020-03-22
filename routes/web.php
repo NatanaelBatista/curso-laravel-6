8be3865c317b3e6b8641ac9fcca3bed315fd9105
Natanael Batista <natanaelbatista@live.com>
2020-03-22 15:15:57 -0300

    Testes com rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::post('/contato', function () {
+    return 'Hello World';
+});
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello World';
 });
